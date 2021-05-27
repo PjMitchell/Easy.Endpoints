93a5e6424dbd9cf73765059c08d7cbb16b3dcd0b
Peter Mitchell <peterjames.m@gmail.com>
2021-05-27 21:11:16 +0100

    Enchanced Endpoint Option (#7)

---
+++ src/Easy.Endpoints/ManifestBuilder/AuthEndpointMetaDataDeclaration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Easy.Endpoints
+{
+    /// <summary>
+    /// Gets Auth Meta data for declared endpoint
+    /// </summary>
+    public class AuthEndpointMetaDataDeclaration : IEndpointMetaDataDeclaration
+    {
+        /// <summary>
+        /// Gets Auth Meta data for declared endpoint
+        /// </summary>
+        /// <param name="declaredEndpoint">Declared endpoint</param>
+        /// <returns>All Auth Meta data for declared endpoint</returns>
+        public IEnumerable<object> GetMetaDataFromDeclaredEndpoint(TypeInfo declaredEndpoint)
+        {
+            return declaredEndpoint.GetCustomAttributes().Where(t => t is IAuthorizeData || t is IAllowAnonymous);            
+        }
+    }
+}
---
+++ src/Easy.Endpoints/ManifestBuilder/IEndpointMetaDataDeclaration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Easy.Endpoints
+{
+    /// <summary>
+    /// Gets Meta data from Declared Endpoint TypeInfo
+    /// </summary>
+    public interface IEndpointMetaDataDeclaration
+    {
+        /// <summary>
+        /// Gets Meta data from Declared Endpoint TypeInfo
+        /// </summary>
+        /// <param name="declaredEndpoint">Declared Endpoint TypeInfo</param>
+        /// <returns>Endpoint Meta data for declared Endpoint</returns>
+        IEnumerable<object> GetMetaDataFromDeclaredEndpoint(TypeInfo declaredEndpoint);
+    }
+}
