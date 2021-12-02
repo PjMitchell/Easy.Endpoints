68efc20097053bdfc85a1d0752df7cf4c325d3a9
PjMitchell <peter@hilltop-digital.co.uk>
2021-12-02 18:53:53 +0000

    Initial work

---
+++ analyzer/Easy.Endpoints.Analyzer.Test/EasyEndpointAnalyzerUnitTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Easy.Endpoints.Analyzer.Test
+{
+    public class EasyEndpointAnalyzerUnitTest
+    {
+        [Fact]
+        public async Task EmptyCode_ReturnsNoErrors()
+        {
+            var test = @"";
+
+            await EasyVerify<EasyEndpointAnalyzerAnalyzer>.VerifyAnalyzerAsync(test);
+        }
+
+        [Fact]
+        public async Task NoValidMethod_ReturnsNoHandlerMethod()
+        {
+            var test = @"
+    using Easy.Endpoints;
+    
+    namespace ConsoleApplication1
+    {
+        public class GetTestEndpoint : IEndpoint
+        {   
+            public void Nope()
+            {
+            }
+        }
+    }";
+
+
+
+            var expected = EasyVerify<EasyEndpointAnalyzerAnalyzer>.Diagnostic(EasyEndpointWarnings.Empty)
+                .WithLocation(6,22)
+                .WithArguments("GetTestEndpoint");
+            await EasyVerify<EasyEndpointAnalyzerAnalyzer>.VerifyAnalyzerAsync(test, expected);
+        }
+
+
+        [Fact]
+        public async Task MultipleValidMethod_ReturnsWarningForEach()
+        {
+            var test = @"
+    using Easy.Endpoints;
+    
+    namespace ConsoleApplication1
+    {
+        public class GetTestEndpoint : IEndpoint
+        {   
+            public void Handle()
+            {
+            }
+
+            public void Handle(int id)
+            {
+            }
+        }
+    }";
+
+
+
+            var expected1 = EasyVerify<EasyEndpointAnalyzerAnalyzer>.Diagnostic(EasyEndpointWarnings.Multiple)
+                .WithLocation(8, 13)
+                .WithArguments("GetTestEndpoint", 2);
+
+            var expected2 = EasyVerify<EasyEndpointAnalyzerAnalyzer>.Diagnostic(EasyEndpointWarnings.Multiple)
+                .WithLocation(12, 13)
+                .WithArguments("GetTestEndpoint", 2);
+            await EasyVerify<EasyEndpointAnalyzerAnalyzer>.VerifyAnalyzerAsync(test, expected1, expected2);
+        }
+    }
+}
---
+++ analyzer/Easy.Endpoints.Analyzer/AnalyzerReleases.Shipped.md
@@ -0,0 +1,11 @@
+﻿; Shipped analyzer releases
+## Release 1.0
+
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|--------------------
+EE1001  |  Usage   |  Warning | Empty IEndpoint
+EE1002  |  Usage   |  Warning | Multiple Methods in IEndpoint
+EE1101  |  Naming   |  Warning | Sync For HandleAsync
+EE1102  |  Naming   |  Warning | Async For Handle
